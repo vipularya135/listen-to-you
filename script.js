fb9ed355a5cedc6208ad7be3e221d686ffed2401
vipul arya <110167247+vipularya135@users.noreply.github.com>
2023-05-06 10:19:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function goToEnglishPage() {
+    window.location.href = "english.html";
+}
+
+function goToBengaliPage() {
+    window.location.href = "bengali.html";
+}
+
+function goToGujaratiPage() {
+    window.location.href = "gujarati.html";
+}
+
+function goToHindiPage() {
+    window.location.href = "hindi.html";
+}
+
+function goToTeluguPage() {
+    window.location.href = "telugu.html";
+}
+
+function goToKannadaPage() {
+    window.location.href = "kannada.html";
+}
+
+function goToTamilPage() {
+    window.location.href = "tamil.html";
+}
+
+function goToMalayalamPage() {
+    window.location.href = "malayalam.html";
+}
+function gototwoindex() {
+    window.location.href = "2index.html";
+}
+
+
